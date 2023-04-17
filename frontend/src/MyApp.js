9e2413b27c67e191526972452894a5de424ce58a
andylimm0606 <andylimm0606@gmail.com>
2023-04-17 01:47:28 -0700

    final

---
+++ frontend/src/MyApp.js
@@ -0,0 +1,93 @@
+import React, {useState, useEffect} from 'react'
+import Table from './Table'
+import Form from './Form';
+import axios from 'axios';
+
+
+
+function MyApp() { 
+  const [characters, setCharacters] = useState([]);
+
+  async function fetchAll(){
+    try {
+      const response = await axios.get('http://localhost:8000/users');
+      return response.data.users_list;     
+    }
+    catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+    }
+ }
+
+  async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:8000/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+ }
+
+ useEffect(() => {
+  fetchAll().then( result => {
+    if (result)
+    setCharacters(result);
+  });
+}, [] );
+
+ function updateList(person) { 
+  makePostCall(person).then( result => {
+  if (result && result.status === 201)
+      setCharacters([...characters, result.data] );
+  });
+}
+
+async function removeOneCharacter (index) {
+  try {
+    const toDelete = characters.filter((character, i) => {
+      return i === index
+    });
+    axios.delete ('http://localhost:8000/users/' + toDelete[0].id);
+
+    const updated = characters.filter((character, i) => {
+           return i !== index;
+         });
+    setCharacters(updated);
+
+  }
+  catch (error) {
+    console.log(error);
+    return false;
+  }
+
+  // const response = await axios.delete('http://localhost:8000/users/' + characters[index].id);
+  // if (response.status === 204) {
+  //   const updated = characters.filter((character, i) => {
+  //     return i !== index;
+  //   });
+  //   setCharacters(updated);
+    
+  // }
+  
+  //const updated = characters.filter((character, i) => {
+  //  return i !== index
+  //});
+  //setCharacters(updated);
+  
+}
+
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  )
+}
+
+
+
+
+export default MyApp;
